6777383c6d51c672a8fae7318c9ec4b692345631
Yue Huang <yh548@cam.ac.uk>
2025-05-22 11:09:54 +0100

    Cannot simplify cfg too early

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -63,7 +63,6 @@ void initPipeline(sys::PassManager &pm) {
   pm.addPass<sys::Mem2Reg>();
   pm.addPass<sys::Alias>();
   pm.addPass<sys::RegularFold>();
-  pm.addPass<sys::SimplifyCFG>();
   pm.addPass<sys::DCE>();
   pm.addPass<sys::CanonicalizeLoop>(/*lcssa=*/ true);
   pm.addPass<sys::LoopRotate>();
